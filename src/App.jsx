faf206b7dabc8055ef686edbe0528224fa32f765
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-08-29 19:46:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import axios from "axios"
+import { useRef, useState } from "react"
+import WheaterInformation from "./components/weatherInformation/weatherInformation"
+
+export default function App() {
+  const [weather, setWeather] = useState({})
+  const inputRef = useRef()
+
+  async function searchCity() {
+    const city = inputRef.current.value
+    const key = 'a22c2119ff0796b794879990f515115f'
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+
+    const apiInfo = await axios.get(url)
+    setWeather(apiInfo.data)
+    console.log(weather)
+  }
+
+  
+
+  return (
+    <>
+     <div>
+        <h1>DevClub Previsão do Tempo</h1>
+        <input ref={inputRef} type="text" placeholder="Digite o nome da cidade"/>
+        <button onClick={searchCity}>Buscar</button>
+
+        <WheaterInformation/>
+     </div>
+    </>
+  )
+}
+
---
+++ src/components/weatherInformation/weatherInformation.jsx
@@ -0,0 +1,7 @@
+export default function WheaterInformation() {
+  return (
+    <div>
+      <h2>meu comp</h2>
+    </div>
+  )
+}
